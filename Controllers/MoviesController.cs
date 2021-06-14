5159ba03a66d6b26acdbc3b601e6859d38530233
muhammad refda <muhammadrefda@gmail.com>
2021-06-14 20:41:19 +0700

    Add MVC structure

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyApplication.Models;
+
+namespace VidlyApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
