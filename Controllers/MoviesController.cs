8bad7f2120a3ee001b58158a85470ded2c62655d
muhammad refda <muhammadrefda@gmail.com>
2021-06-17 17:15:02 +0700

    update the movie controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,6 +11,22 @@ namespace VidlyApplication.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
