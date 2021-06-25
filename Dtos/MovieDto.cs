2f57ed64bcd1144e8689060726a21bfd9d9db2c5
muhammad refda <muhammadrefda@gmail.com>
2021-06-25 14:19:55 +0700

    Create API Rent movie

--- Dtos/MovieDto.cs
+++ Dtos/MovieDto.cs
@@ -23,5 +23,6 @@ namespace VidlyApplication.Dtos
         public DateTime ReleaseDate { get; set; }
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
+        public byte NumberAvailable { get; set; }
     }
 }
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -10,6 +9,10 @@ namespace VidlyApplication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
         [Required]
         [StringLength(255)]
         public string DrivingLicence { get; set; }
@@ -22,21 +25,4 @@ namespace VidlyApplication.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
