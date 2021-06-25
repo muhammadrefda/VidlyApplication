3114f46729c26dc707d20533ad8be6f4e6477f84
muhammad refda <muhammadrefda@gmail.com>
2021-06-25 14:18:34 +0700

    Add Number Available to Movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -30,6 +30,8 @@ namespace VidlyApplication.Models
         [Range(1, 20)]
         [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
-        
+
+        public byte NumberAvailable { get; set; }
+
     }
 }
